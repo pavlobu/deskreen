219202f880a55cb5b95c9e38e95b4d811782f12a
Walker <walker@praiseweb.com.br>
2021-01-31 20:58:20 -0300

    doc: add comment about core-js import

--- app/client/src/polyfills.tsx
+++ app/client/src/polyfills.tsx
@@ -2,5 +2,6 @@
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
-
+// probably is better to load polyfills on demand,
+// browser tests are required to detect which polyfills are needed
 import "core-js";
