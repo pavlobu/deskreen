9dfeb85b44d13d33d4855c9e7da109f5f9d5c1b2
Walker <walker@praiseweb.com.br>
2021-01-31 21:15:55 -0300

    style: use single quote

--- app/client/src/polyfills.tsx
+++ app/client/src/polyfills.tsx
@@ -4,4 +4,4 @@ import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 // probably is better to load polyfills on demand,
 // browser tests are required to detect which polyfills are needed
-import "core-js";
+import 'core-js';
