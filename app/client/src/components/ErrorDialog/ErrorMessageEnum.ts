997a63d24df4f97c56008dba61a976de8a2c3fa2
Pavlo Buidenkov <pavlobu@gmail.com>
2021-02-04 15:32:03 +0200

    add WebRTC error on client

--- app/client/src/components/ErrorDialog/ErrorMessageEnum.ts
+++ app/client/src/components/ErrorDialog/ErrorMessageEnum.ts
@@ -3,4 +3,5 @@ export enum ErrorMessage {
   DENY_TO_CONNECT = 'You were not allowed to connect.',
   DISCONNECTED = 'You were disconnected.',
   NOT_ALLOWED = 'You were not allowed to connect.',
+  WEBRTC_ERROR = 'WebRTC error occurred.'
 }
