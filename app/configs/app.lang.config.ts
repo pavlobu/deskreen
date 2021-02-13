9d0ee4679e76c39dccdea28bfaa73ad2f25ad60b
Sebastian Schauenburg <s.schauenburg@gmail.com>
2021-02-13 22:15:25 +0100

    Added Dutch translation

--- app/configs/app.lang.config.ts
+++ app/configs/app.lang.config.ts
@@ -3,10 +3,11 @@
 export default {
   fallbackLng: 'en',
   namespace: 'translation',
-  languages: ['ru', 'en', 'ua'],
+  languages: ['ru', 'en', 'ua', 'nl'],
   langISOKeyToLangFullNameMap: {
     en: 'English',
     ru: 'Русский',
     ua: 'Українська',
+    nl: 'Nederlands',
   },
 };
