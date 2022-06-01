95b67f9bcf04d612097be1da1bff78cf9507000c
Pavlo Buidenkov <pavlobu@gmail.com>
2022-06-02 01:19:02 +0200

    add French language

--- app/configs/app.lang.config.ts
+++ app/configs/app.lang.config.ts
@@ -16,6 +16,7 @@ export default {
     'ko',
     'it',
     'ja',
+    'fr',
   ],
   langISOKeyToLangFullNameMap: {
     en: 'English',
@@ -26,6 +27,7 @@ export default {
     de: 'Deutsch',
     fi: 'Suomi',
     it: 'Italiano',
+    fr: 'Français',
     ko: '한국어',
     zh_CN: '简体中文',
     zh_TW: '繁體中文',
